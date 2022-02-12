13dc18fc2ecde12ec439cbdcee2f20151cf9e841
karenrds <karen.rodrigues30@gmail.com>
2022-02-12 19:27:00 +0000

    Removed commented code- plateau

--- src/main/java/com/techreturners/Plateau.java
+++ src/main/java/com/techreturners/Plateau.java
@@ -17,29 +17,4 @@ public class Plateau {
             }
         }
     }
-    /*
-    private String name;
-    private ArrayList<Rover> roverList;
-
-
-    public Plateau(String name) {
-        this.name = name;
-        this.roverList = new ArrayList<Rover>();
-    }
-
-    public boolean addRover(Rover rover){
-        if (rover==null || roverList.contains(rover)) {
-            return false;
-        }
-        roverList.add(rover);
-        return true;
-    } */
-/*
-    public void addRovers(Rover[] rovers){
-        for(int i = 0; i < rovers.length; i++){
-            roverList.add(rovers[i]);
-        }
-    }*/
-
-
 }
